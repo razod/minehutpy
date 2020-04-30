36f9d0bc61aa2d3ce8c6826281942d532214f29c
razod <masonboulier@gmail.com>
2020-04-30 00:00:35 -0400

    repair files

--- minehut.py
+++ minehut.py
@@ -178,4 +178,15 @@ class minehut():
         response = requests.post(url, headers=headers ,verify=False)
         j = json.loads(response.content)
         return j
+
+    def repairServerFiles(self, token, xsessionid, server_id):
+        headers = {
+            'content-type': 'application/json; charset=utf-8',
+            'x-session-id': xsessionid,
+            'Authorization': token
+        }
+        url = "https://api.minehut.com/servers/" + server_id + "/repair_files"
+        response = requests.post(url, headers=headers ,verify=False)
+        j = json.loads(response.content)
+        return j
     
