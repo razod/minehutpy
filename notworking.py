e77dcdc55e8fb812e3d83590d72cc679ca97b99f
razod <masonboulier@gmail.com>
2020-04-30 00:47:36 -0400

    :( listserverrootfiles aint wokring

---
+++ notworking.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+# this is a list of the currently not working api endpoints
+
+class notworking():
+    def __init__(self):
+        pass
+    def listRootFiles(self, token, xsessionid, server_id):
+        headers = {
+            'content-type': 'application/json; charset=utf-8',
+            'x-session-id': xsessionid,
+            'Authorization': token
+        }
+        url = "https://api.minehut.com/server/files/" + server_id + "/list"
+        response = requests.get(url, headers=headers ,verify=False)
+        j = json.loads(response.content)
+        return j
